c61f7ba4c713c3f70008c4eb651045c54c1c8847
Nicholas Ng <onenylxus@gmail.com>
2023-04-07 11:44:35 +0800

    Simplify module test

--- src/routines/1s/saxpy.ts
+++ src/routines/1s/saxpy.ts
@@ -2,10 +2,10 @@
 import { Integer, I } from '../../types/integer';
 import { Single, S } from '../../types/single';
 
-// SNRM2 routine
-const snrm2 = (n: number, sa: number, sx: Array<number>, incx: number, sy: Array<number>, incy: number): void => {
+// SAXPY routine
+const saxpy = (n: number, sa: number, sx: Array<number>, incx: number, sy: Array<number>, incy: number): void => {
 
 };
 
 // Export
-export default snrm2;
+export default saxpy;
--- utils/random.ts
+++ utils/random.ts
@@ -1,5 +1,5 @@
 // Random integer function
-const random = (min: number = Number.MIN_SAFE_INTEGER, max: number = Number.MAX_SAFE_INTEGER) : number => {
+const random = (min: number = Number.MIN_SAFE_INTEGER, max: number = Number.MAX_SAFE_INTEGER): number => {
   return min + Math.random() * (max - min);
 };
 
