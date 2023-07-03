1a198866c892559a35ead771ff0502e73cfec079
Nicholas Ng <onenylxus@gmail.com>
2023-07-03 21:51:08 +0800

    Simple generic type tests

--- tests/types/double.test.ts
+++ tests/types/double.test.ts
@@ -26,4 +26,71 @@ describe('Double type', () => {
     instance.clear();
     expect(instance.get()).toStrictEqual(0);
   });
+
+  it('should add value correctly', () => {
+    instance.set(value);
+    instance.add(1);
+    expect(instance.get()).toStrictEqual(value + 1);
+  });
+
+  it('should subtract value correctly', () => {
+    instance.set(value);
+    instance.sub(1);
+    expect(instance.get()).toStrictEqual(value - 1);
+  });
+
+  it('should multiply value correctly', () => {
+    instance.set(value);
+    instance.mul(2);
+    expect(instance.get()).toStrictEqual(value * 2);
+  });
+
+  it('should divide value correctly', () => {
+    instance.set(value);
+    instance.div(2);
+    expect(instance.get()).toStrictEqual(value / 2);
+  });
+
+  it('should exponent value correctly', () => {
+    instance.set(value);
+    instance.exp(2);
+    expect(instance.get()).toStrictEqual(value ** 2);
+  });
+
+  it('should compare equal to value correctly', () => {
+    instance.set(value);
+    expect(instance.eq(value + 1) === (instance.get() === value + 1)).toBeTruthy();
+    expect(instance.eq(value) === (instance.get() === value)).toBeTruthy();
+    expect(instance.eq(value - 1) === (instance.get() === value - 1)).toBeTruthy();
+  });
+
+  it('should compare not equal to value correctly', () => {
+    expect(instance.ne(value + 1) === (instance.get() !== value + 1)).toBeTruthy();
+    expect(instance.ne(value) === (instance.get() !== value)).toBeTruthy();
+    expect(instance.ne(value - 1) === (instance.get() !== value - 1)).toBeTruthy();
+  });
+
+  it('should compare greater than value correctly', () => {
+    expect(instance.gt(value + 1) === (instance.get() > value + 1)).toBeTruthy();
+    expect(instance.gt(value) === (instance.get() > value)).toBeTruthy();
+    expect(instance.gt(value - 1) === (instance.get() > value - 1)).toBeTruthy();
+  });
+
+  it('should compare less than value correctly', () => {
+    expect(instance.lt(value + 1) === (instance.get() < value + 1)).toBeTruthy();
+    expect(instance.lt(value) === (instance.get() < value)).toBeTruthy();
+    expect(instance.lt(value - 1) === (instance.get() < value - 1)).toBeTruthy();
+  });
+
+  it('should compare greater than or equal to value correctly', () => {
+    expect(instance.ge(value + 1) === (instance.get() >= value + 1)).toBeTruthy();
+    expect(instance.ge(value) === (instance.get() >= value)).toBeTruthy();
+    expect(instance.ge(value - 1) === (instance.get() >= value - 1)).toBeTruthy();
+  });
+
+  it('should compare less than or equal to value correctly', () => {
+    expect(instance.le(value + 1) === (instance.get() <= value + 1)).toBeTruthy();
+    expect(instance.le(value) === (instance.get() <= value)).toBeTruthy();
+    expect(instance.le(value - 1) === (instance.get() <= value - 1)).toBeTruthy();
+  });
 });
--- tests/types/single.test.ts
+++ tests/types/single.test.ts
@@ -1,5 +1,6 @@
 // Import
 import { Single, S } from '../../src/types/single';
+import { MAX_SP_INT, MIN_SP_INT } from '../../utils/limit';
 import random from '../../utils/random';
 import relerr from '../../utils/relerr';
 
@@ -10,7 +11,7 @@ let instance: S;
 // Single type test
 describe('Single type', () => {
   beforeAll(() => {
-    value = random();
+    value = random(MIN_SP_INT, MAX_SP_INT);
     instance = new Single();
   });
 
@@ -26,5 +27,72 @@ describe('Single type', () => {
   it('should clear value to zero', () => {
     instance.clear();
     expect(instance.get()).toStrictEqual(0);
+  });
+
+  it('should add value correctly', () => {
+    instance.set(value);
+    instance.add(1);
+    expect(relerr(instance.get(), value + 1)).toBeLessThan(10 ** -7);
+  });
+
+  it('should subtract value correctly', () => {
+    instance.set(value);
+    instance.sub(1);
+    expect(relerr(instance.get(), value - 1)).toBeLessThan(10 ** -7);
+  });
+
+  it('should multiply value correctly', () => {
+    instance.set(value);
+    instance.mul(2);
+    expect(relerr(instance.get(), value * 2)).toBeLessThan(10 ** -7);
+  });
+
+  it('should divide value correctly', () => {
+    instance.set(value);
+    instance.div(2);
+    expect(relerr(instance.get(), value / 2)).toBeLessThan(10 ** -7);
+  });
+
+  it('should exponent value correctly', () => {
+    instance.set(value);
+    instance.exp(2);
+    expect(relerr(instance.get(), value ** 2)).toBeLessThan(10 ** -7);
+  });
+
+  it('should compare equal to value correctly', () => {
+    instance.set(value);
+    expect(instance.eq(value + 1) === (instance.get() === value + 1)).toBeTruthy();
+    expect(instance.eq(value) === (instance.get() === value)).toBeTruthy();
+    expect(instance.eq(value - 1) === (instance.get() === value - 1)).toBeTruthy();
+  });
+
+  it('should compare not equal to value correctly', () => {
+    expect(instance.ne(value + 1) === (instance.get() !== value + 1)).toBeTruthy();
+    expect(instance.ne(value) === (instance.get() !== value)).toBeTruthy();
+    expect(instance.ne(value - 1) === (instance.get() !== value - 1)).toBeTruthy();
+  });
+
+  it('should compare greater than value correctly', () => {
+    expect(instance.gt(value + 1) === (instance.get() > value + 1)).toBeTruthy();
+    expect(instance.gt(value) === (instance.get() > value)).toBeTruthy();
+    expect(instance.gt(value - 1) === (instance.get() > value - 1)).toBeTruthy();
+  });
+
+  it('should compare less than value correctly', () => {
+    expect(instance.lt(value + 1) === (instance.get() < value + 1)).toBeTruthy();
+    expect(instance.lt(value) === (instance.get() < value)).toBeTruthy();
+    expect(instance.lt(value - 1) === (instance.get() < value - 1)).toBeTruthy();
+  });
+
+  it('should compare greater than or equal to value correctly', () => {
+    expect(instance.ge(value + 1) === (instance.get() >= value + 1)).toBeTruthy();
+    expect(instance.ge(value) === (instance.get() >= value)).toBeTruthy();
+    expect(instance.ge(value - 1) === (instance.get() >= value - 1)).toBeTruthy();
+  });
+
+  it('should compare less than or equal to value correctly', () => {
+    expect(instance.le(value + 1) === (instance.get() <= value + 1)).toBeTruthy();
+    expect(instance.le(value) === (instance.get() <= value)).toBeTruthy();
+    expect(instance.le(value - 1) === (instance.get() <= value - 1)).toBeTruthy();
   });
 });
