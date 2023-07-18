47dbf564026286b12637e43afefd39fe39e75def
Nicholas Ng <onenylxus@gmail.com>
2023-07-18 23:18:00 +0800

    SSWAP routine v2

--- src/routines/1s/sswap.ts
+++ src/routines/1s/sswap.ts
@@ -1,9 +1,90 @@
+// Import
+import { Integer, I } from '../../types/integer';
+import { Single, S } from '../../types/single';
+import { SingleArray, SA } from '../../types/singleArray';
+
+// Math library snippets
+const { abs } = Math;
+
 // SSWAP routine
-const sswap = (): FVoid => {
+const sswap = (n: FInteger, sx: FRealArray, incx: FInteger, sy: FRealArray, incy: FInteger): FVoid => {
   // Copyright (c) 1992-2013 The University of Tennessee and The University of Tennessee Research Foundation. All rights reserved.
   // Copyright (c) 2000-2013 The University of California Berkeley. All rights reserved.
   // Copyright (c) 2006-2013 The University of Colorado Denver. All rights reserved.
   // Copyright (c) 2023      Nicholas Ng. All rights reserved.
+
+  // Scalar arguments
+  const _n: I = new Integer();
+  const _sx: SA = new SingleArray(sx, 1 + (n - 1) * abs(incx));
+  const _incx: I = new Integer(incx);
+  const _sy: SA = new SingleArray(sy, 1 + (n - 1) * abs(incy));
+  const _incy: I = new Integer(incy);
+
+  // Resolve function
+  const resolve = (): void => {
+    n = _n.get();
+    sx = _sx.getAll();
+    incx = _incx.get();
+    sy = _sy.getAll();
+    incy = _incy.get();
+  };
+
+  // Local variables
+  const stemp: S = new Single();
+  const ix: I = new Integer();
+  const iy: I = new Integer();
+  const m: I = new Integer();
+  const mp1: I = new Integer();
+  const i: I = new Integer();
+
+  // Process
+  if (_n.le(0)) {
+    return resolve();
+  }
+  if (_incx.eq(1) && _incy.eq(1)) {
+    m.set(_n.get() % 3);
+    if (m.ne(0)) {
+      for (i.set(1); i.le(m.get()); i.add(1)) {
+        stemp.set(_sx.get(i.get()));
+        _sx.set(i.get(), _sy.get(i.get()));
+        _sy.set(i.get(), stemp.get());
+      }
+      if (_n.lt(3)) {
+        return resolve();
+      }
+    }
+    mp1.set(m.get() + 1);
+    for (i.set(mp1.get()); i.le(_n.get()); i.add(3)) {
+      stemp.set(_sx.get(i.get()));
+      _sx.set(i.get(), _sy.get(i.get()));
+      _sy.set(i.get(), stemp.get());
+      stemp.set(_sx.get(i.get() + 1));
+      _sx.set(i.get() + 1, _sy.get(i.get() + 1));
+      _sy.set(i.get() + 1, stemp.get());
+      stemp.set(_sx.get(i.get() + 2));
+      _sx.set(i.get() + 2, _sy.get(i.get() + 2));
+      _sy.set(i.get() + 2, stemp.get());
+    }
+  } else {
+    ix.set(1);
+    iy.set(1);
+    if (_incx.lt(0)) {
+      ix.set((-_n.get() + 1) * _incx.get() + 1);
+    }
+    if (_incy.lt(0)) {
+      iy.set((-_n.get() + 1) * _incy.get() + 1);
+    }
+    for (i.set(1); i.le(_n.get()); i.add(1)) {
+      stemp.set(_sx.get(i.get()));
+      _sx.set(i.get(), _sy.get(i.get()));
+      _sy.set(i.get(), stemp.get());
+      ix.add(_incx.get());
+      iy.add(_incy.get());
+    }
+  }
+
+  // Output
+  resolve();
 };
 
 // Export
