5c6205f16a3ac47759fc5d77513d1161458b102f
Nicholas Ng <onenylxus@gmail.com>
2023-07-18 23:18:38 +0800

    SSCAL routine v2

--- src/routines/1s/sscal.ts
+++ src/routines/1s/sscal.ts
@@ -1,9 +1,69 @@
+// Import
+import { Integer, I } from '../../types/integer';
+import { Single, S } from '../../types/single';
+import { SingleArray, SA } from '../../types/singleArray';
+
+// Math library snippets
+const { abs } = Math;
+
 // SSCAL routine
-const sscal = (): FVoid => {
+const sscal = (n: FInteger, sa: FReal, sx: FRealArray, incx: FInteger): FVoid => {
   // Copyright (c) 1992-2013 The University of Tennessee and The University of Tennessee Research Foundation. All rights reserved.
   // Copyright (c) 2000-2013 The University of California Berkeley. All rights reserved.
   // Copyright (c) 2006-2013 The University of Colorado Denver. All rights reserved.
   // Copyright (c) 2023      Nicholas Ng. All rights reserved.
+
+  // Scalar arguments
+  const _n: I = new Integer(n);
+  const _sa: S = new Single(sa);
+  const _sx: SA = new SingleArray(sx, 1 + (n - 1) * abs(incx));
+  const _incx: I = new Integer(incx);
+
+  // Resolve function
+  const resolve = (): void => {
+    n = _n.get();
+    sa = _sa.get();
+    sx = _sx.getAll();
+    incx = _incx.get();
+  };
+
+  // Local variables
+  const i: I = new Integer();
+  const m: I = new Integer();
+  const mp1: I = new Integer();
+  const nincx: I = new Integer();
+
+  // Process
+  if (_n.le(0) || _incx.le(0) || _sa.eq(1)) {
+    return resolve();
+  }
+  if (_incx.eq(1)) {
+    m.set(_n.get() % 5);
+    if (m.ne(0)) {
+      for (i.set(1); i.le(m.get()); i.add(1)) {
+        _sx.set(i.get(), _sa.get() * _sx.get(i.get()));
+      }
+      if (_n.lt(5)) {
+        return resolve();
+      }
+    }
+    mp1.set(m.get() + 1);
+    for (i.set(mp1.get()); i.le(_n.get()); i.add(5)) {
+      _sx.set(i.get(), _sa.get() * _sx.get(i.get()));
+      _sx.set(i.get() + 1, _sa.get() * _sx.get(i.get() + 1));
+      _sx.set(i.get() + 2, _sa.get() * _sx.get(i.get() + 2));
+      _sx.set(i.get() + 3, _sa.get() * _sx.get(i.get() + 3));
+      _sx.set(i.get() + 4, _sa.get() * _sx.get(i.get() + 4));
+    }
+  } else {
+    nincx.set(_n.get() * _incx.get());
+    for (i.set(1); i.le(nincx.get()); i.add(_incx.get())) {
+      _sx.set(i.get(), _sa.get() * _sx.get(i.get()));
+    }
+  }
+
+  // Output
+  resolve();
 };
 
 // Export
