979b966407b3abe75d56d5dc933604c4e669d77d
Nicholas Ng <onenylxus@gmail.com>
2023-10-22 23:07:14 +0800

    Compare function

--- tests/routines/1s/scopy.test.ts
+++ tests/routines/1s/scopy.test.ts
@@ -1,5 +1,6 @@
 // Import
 import blas from '../../../src/index';
+import compare from '../../../utils/compare';
 
 // Routine snippet
 const { scopy } = blas;
@@ -26,12 +27,12 @@ describe('SCOPY routine test', () => {
     result = scopy({ n, sx, incx, sy, incy });
 
     // Output
-    expect(result._ret).toBeUndefined();
-    expect(result.n).toStrictEqual(4);
-    expect(result.sx).toStrictEqual([1, 2, 3, 4]);
-    expect(result.incx).toStrictEqual(1);
-    expect(result.sy).toStrictEqual([1, 2, 3, 4]);
-    expect(result.incy).toStrictEqual(1);
+    expect(compare(result._ret, undefined)).toBeTruthy();
+    expect(compare(result.n, 4)).toBeTruthy();
+    expect(compare(result.sx, [1, 2, 3, 4])).toBeTruthy();
+    expect(compare(result.incx, 1)).toBeTruthy();
+    expect(compare(result.sy, [1, 2, 3, 4])).toBeTruthy();
+    expect(compare(result.incy, 1)).toBeTruthy();
   });
 
   it('Case 2: n{10},sx[10],incx{1},sy[10],incy{1}', () => {
@@ -46,12 +47,12 @@ describe('SCOPY routine test', () => {
     result = scopy({ n, sx, incx, sy, incy });
 
     // Output
-    expect(result._ret).toBeUndefined();
-    expect(result.n).toStrictEqual(10);
-    expect(result.sx).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    expect(result.incx).toStrictEqual(1);
-    expect(result.sy).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    expect(result.incy).toStrictEqual(1);
+    expect(compare(result._ret, undefined)).toBeTruthy();
+    expect(compare(result.n, 10)).toBeTruthy();
+    expect(compare(result.sx, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBeTruthy();
+    expect(compare(result.incx, 1)).toBeTruthy();
+    expect(compare(result.sy, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBeTruthy();
+    expect(compare(result.incy, 1)).toBeTruthy();
   });
 
   it('Case 3: n{7},sx[7],incx{1},sy[7],incy{1}', () => {
@@ -66,12 +67,12 @@ describe('SCOPY routine test', () => {
     result = scopy({ n, sx, incx, sy, incy });
 
     // Output
-    expect(result._ret).toBeUndefined();
-    expect(result.n).toStrictEqual(7);
-    expect(result.sx).toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
-    expect(result.incx).toStrictEqual(1);
-    expect(result.sy).toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
-    expect(result.incy).toStrictEqual(1);
+    expect(compare(result._ret, undefined)).toBeTruthy();
+    expect(compare(result.n, 7)).toBeTruthy();
+    expect(compare(result.sx, [1, 2, 3, 4, 5, 6, 7])).toBeTruthy();
+    expect(compare(result.incx, 1)).toBeTruthy();
+    expect(compare(result.sy, [1, 2, 3, 4, 5, 6, 7])).toBeTruthy();
+    expect(compare(result.incy, 1)).toBeTruthy();
   });
 
   it('Case 4: n{5},sx[9],incx{2},sy[9],incy{2}', () => {
@@ -86,12 +87,12 @@ describe('SCOPY routine test', () => {
     result = scopy({ n, sx, incx, sy, incy });
 
     // Output
-    expect(result._ret).toBeUndefined();
-    expect(result.n).toStrictEqual(5);
-    expect(result.sx).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    expect(result.incx).toStrictEqual(2);
-    expect(result.sy).toStrictEqual([1, 0, 3, 0, 5, 0, 7, 0, 9]);
-    expect(result.incy).toStrictEqual(2);
+    expect(compare(result._ret, undefined)).toBeTruthy();
+    expect(compare(result.n, 5)).toBeTruthy();
+    expect(compare(result.sx, [1, 2, 3, 4, 5, 6, 7, 8, 9])).toBeTruthy();
+    expect(compare(result.incx, 2)).toBeTruthy();
+    expect(compare(result.sy, [1, 0, 3, 0, 5, 0, 7, 0, 9])).toBeTruthy();
+    expect(compare(result.incy, 2)).toBeTruthy();
   });
 
   it('Case 5: n{5},sx[9],incx{2},sy[9],incy{-1}', () => {
@@ -106,12 +107,12 @@ describe('SCOPY routine test', () => {
     result = scopy({ n, sx, incx, sy, incy });
 
     // Output
-    expect(result._ret).toBeUndefined();
-    expect(result.n).toStrictEqual(5);
-    expect(result.sx).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    expect(result.incx).toStrictEqual(2);
-    expect(result.sy).toStrictEqual([9, 7, 5, 3, 1, 0, 0, 0, 0]);
-    expect(result.incy).toStrictEqual(-1);
+    expect(compare(result._ret, undefined)).toBeTruthy();
+    expect(compare(result.n, 5)).toBeTruthy();
+    expect(compare(result.sx, [1, 2, 3, 4, 5, 6, 7, 8, 9])).toBeTruthy();
+    expect(compare(result.incx, 2)).toBeTruthy();
+    expect(compare(result.sy, [9, 7, 5, 3, 1, 0, 0, 0, 0])).toBeTruthy();
+    expect(compare(result.incy, -1)).toBeTruthy();
   });
 
   it('Case 6: n{5},sx[9],incx{-2},sy[9],incy{1}', () => {
@@ -126,12 +127,12 @@ describe('SCOPY routine test', () => {
     result = scopy({ n, sx, incx, sy, incy });
 
     // Output
-    expect(result._ret).toBeUndefined();
-    expect(result.n).toStrictEqual(5);
-    expect(result.sx).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    expect(result.incx).toStrictEqual(-2);
-    expect(result.sy).toStrictEqual([9, 7, 5, 3, 1, 0, 0, 0, 0]);
-    expect(result.incy).toStrictEqual(1);
+    expect(compare(result._ret, undefined)).toBeTruthy();
+    expect(compare(result.n, 5)).toBeTruthy();
+    expect(compare(result.sx, [1, 2, 3, 4, 5, 6, 7, 8, 9])).toBeTruthy();
+    expect(compare(result.incx, -2)).toBeTruthy();
+    expect(compare(result.sy, [9, 7, 5, 3, 1, 0, 0, 0, 0])).toBeTruthy();
+    expect(compare(result.incy, 1)).toBeTruthy();
   });
 
   it('Case 7: n{0},sx[4],incx{1},sy[4],incy{1}', () => {
@@ -146,11 +147,11 @@ describe('SCOPY routine test', () => {
     result = scopy({ n, sx, incx, sy, incy });
 
     // Output
-    expect(result._ret).toBeUndefined();
-    expect(result.n).toStrictEqual(0);
-    expect(result.sx).toStrictEqual([1, 2, 3, 4]);
-    expect(result.incx).toStrictEqual(1);
-    expect(result.sy).toStrictEqual([0, 0, 0, 0]);
-    expect(result.incy).toStrictEqual(1);
+    expect(compare(result._ret, undefined)).toBeTruthy();
+    expect(compare(result.n, 0)).toBeTruthy();
+    expect(compare(result.sx, [1, 2, 3, 4])).toBeTruthy();
+    expect(compare(result.incx, 1)).toBeTruthy();
+    expect(compare(result.sy, [0, 0, 0, 0])).toBeTruthy();
+    expect(compare(result.incy, 1)).toBeTruthy();
   });
 });
---
+++ utils/compare.ts
@@ -0,0 +1,25 @@
+// Compare function
+const compare = (left: any, right: any, precision: number = 10e-5): boolean => {
+  // Array iteration
+  if (left instanceof Array && right instanceof Array) {
+    return left.length === right.length && left.every((v, i) => compare(v, right[i], precision));
+  }
+
+  // Dual check
+  if (typeof left === 'object' && typeof right === 'object') {
+    if (typeof left.r === 'number' && typeof left.i === 'number' && typeof right.r === 'number' && typeof right.i === 'number') {
+      return compare(left.r, right.r, precision) && compare(left.i, right.i, precision);
+    }
+  }
+
+  // Number check with precision
+  if (typeof left === 'number' && typeof right === 'number') {
+    return Math.abs((left - right) / ((right !== 0) ? right : 1)) <= precision;
+  }
+
+  // Fallback to strict equal check
+  return left === right;
+};
+
+// Export
+export default compare;
