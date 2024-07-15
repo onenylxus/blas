8e6be420c6c43dd2c04e0cf1f7f36a3f6cab780e
Nicholas Ng <onenylxus@gmail.com>
2024-07-16 00:46:04 +0800

    Isolate test data from ISAMAX routine test

--- tests/routines/1s/isamax.test.ts
+++ tests/routines/1s/isamax.test.ts
@@ -2,99 +2,28 @@
 import { isClose, isEqual, isPerf } from '../../testers';
 import blas from '../../../src/index';
 import perf from '../../../utils/performance';
-
-// Routine snippet
-const { isamax } = blas;
-
-// Define test variables
-let n: number;
-let sx: number[];
-let incx: number;
+import data from '../../data/1s/isamax.json';
 
 // ISAMAX routine test
 describe('ISAMAX routine test', () => {
-  it('Case 1: n{6},sx[6],incx{1}', () => {
-    // Input
-    n = 6;
-    sx = [1, 2, 3, 4, 5, 6];
-    incx = 1;
+  let count = 1;
 
-    // Apply routine
-    const { result, time } = perf(isamax, { n, sx, incx });
+  data.forEach((node) => {
+    const n = node.input.n;
+    const sx = node.input.sx;
+    const incx = node.input.incx;
 
-    // Output
-    isClose(result._ret, 6);
-    isEqual(result.n, 6);
-    isClose(result.sx, [1, 2, 3, 4, 5, 6]);
-    isEqual(result.incx, 1);
-    isPerf(time);
-  });
+    const _ret = node.output._ret;
 
-  it('Case 2: n{3},sx[6],incx{2}', () => {
-    // Input
-    n = 3;
-    sx = [1, 2, 3, 4, 5, 6];
-    incx = 2;
+    const { result, time } = perf(blas.isamax, { n, sx, incx });
 
-    // Apply routine
-    const { result, time } = perf(isamax, { n, sx, incx });
-
-    // Output
-    isClose(result._ret, 3);
-    isEqual(result.n, 3);
-    isClose(result.sx, [1, 2, 3, 4, 5, 6]);
-    isEqual(result.incx, 2);
-    isPerf(time);
-  });
-
-  it('Case 3: n{6},sx[6],incx{-1}', () => {
-    // Input
-    n = 6;
-    sx = [1, 2, 3, 4, 5, 6];
-    incx = -1;
-
-    // Apply routine
-    const { result, time } = perf(isamax, { n, sx, incx });
-
-    // Output
-    isClose(result._ret, 0);
-    isEqual(result.n, 6);
-    isClose(result.sx, [1, 2, 3, 4, 5, 6]);
-    isEqual(result.incx, -1);
-    isPerf(time);
-  });
-
-  it('Case 4: n{0},sx[6],incx{1}', () => {
-    // Input
-    n = 0;
-    sx = [1, 2, 3, 4, 5, 6];
-    incx = 1;
-
-    // Apply routine
-    const { result, time } = perf(isamax, { n, sx, incx });
-
-    // Output
-    isClose(result._ret, 0);
-    isEqual(result.n, 0);
-    isClose(result.sx, [1, 2, 3, 4, 5, 6]);
-    isEqual(result.incx, 1);
-    isPerf(time);
-  });
-
-  it('Case 5: n{1},sx[6],incx{1}', () => {
-    // Input
-    n = 1;
-    sx = [1, 2, 3, 4, 5, 6];
-    incx = 1;
-
-    // Apply routine
-    const { result, time } = perf(isamax, { n, sx, incx });
-
-    // Output
-    isClose(result._ret, 1);
-    isEqual(result.n, 1);
-    isClose(result.sx, [1, 2, 3, 4, 5, 6]);
-    isEqual(result.incx, 1);
-    isPerf(time);
+    // Run test
+    it(`Case ${count++}: n{${n}},sx[${sx.length}],incx{${incx}}`, () => {
+      isClose(result._ret, _ret);
+      isEqual(result.n, n);
+      isClose(result.sx, sx);
+      isEqual(result.incx, incx);
+      isPerf(time);
+    });
   });
 });
