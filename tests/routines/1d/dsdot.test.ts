0089e92e4c18e31146e9ab3631c26334d99e0715
Nicholas Ng <onenylxus@gmail.com>
2024-07-17 09:25:09 +0800

    Isolate test data from DSDOT routine test

--- tests/routines/1d/dsdot.test.ts
+++ tests/routines/1d/dsdot.test.ts
@@ -1,122 +1,31 @@
 // Import
-import { isClose, isEqual, isPerf } from '../../testers';
+import { isEqual, isPerf } from '../../testers';
 import blas from '../../../src/index';
+import data from '../../data/1d/dsdot.json';
 import perf from '../../../utils/performance';
-
-// Routine snippet
-const { dsdot } = blas;
-
-// Define test variables
-let n: number;
-let sx: number[];
-let incx: number;
-let sy: number[];
-let incy: number;
 
 // DSDOT routine test
 describe('DSDOT routine test', () => {
-  it('Case 1: n{4},sx[4],incx{1},sy[4],incy{1}', () => {
-    // Input
-    n = 4;
-    sx = [1, 2, 3, 4];
-    incx = 1;
-    sy = [5, 6, 7, 8];
-    incy = 1;
+  data.forEach((node, i) => {
+    const n = node.input.n;
+    const sx = node.input.sx;
+    const incx = node.input.incx;
+    const sy = node.input.sy;
+    const incy = node.input.incy;
 
-    // Apply routine
-    const { result, time } = perf(dsdot, { n, sx, incx, sy, incy });
+    const _ret = node.output._ret;
 
-    // Output
-    isClose(result._ret, 70);
-    isEqual(result.n, 4);
-    isClose(result.sx, [1, 2, 3, 4]);
-    isEqual(result.incx, 1);
-    isClose(result.sy, [5, 6, 7, 8]);
-    isEqual(result.incy, 1);
-    isPerf(time);
-  });
+    const { result, time } = perf(blas.dsdot, { n, sx, incx, sy, incy });
 
-  it('Case 2: n{0},sx[4],incx{1},sy[4],incy{1}', () => {
-    // Input
-    n = 0;
-    sx = [1, 2, 3, 4];
-    incx = 1;
-    sy = [5, 6, 7, 8];
-    incy = 1;
-
-    // Apply routine
-    const { result, time } = perf(dsdot, { n, sx, incx, sy, incy });
-
-    // Output
-    isClose(result._ret, 0);
-    isEqual(result.n, 0);
-    isClose(result.sx, [1, 2, 3, 4]);
-    isEqual(result.incx, 1);
-    isClose(result.sy, [5, 6, 7, 8]);
-    isEqual(result.incy, 1);
-    isPerf(time);
-  });
-
-  it('Case 3: n{4},sx[4],incx{2},sy[4],incy{2}', () => {
-    // Input
-    n = 4;
-    sx = [1, 2, 3, 4];
-    incx = 2;
-    sy = [5, 6, 7, 8];
-    incy = 2;
-
-    // Apply routine
-    const { result, time } = perf(dsdot, { n, sx, incx, sy, incy });
-
-    // Output
-    isClose(result._ret, 26);
-    isEqual(result.n, 4);
-    isClose(result.sx, [1, 2, 3, 4]);
-    isEqual(result.incx, 2);
-    isClose(result.sy, [5, 6, 7, 8]);
-    isEqual(result.incy, 2);
-    isPerf(time);
-  });
-
-  it('Case 4: n{2},sx[4],incx{-1},sy[4],incy{-1}', () => {
-    // Input
-    n = 2;
-    sx = [1, 2, 3, 4];
-    incx = -1;
-    sy = [5, 6, 7, 8];
-    incy = -1;
-
-    // Apply routine
-    const { result, time } = perf(dsdot, { n, sx, incx, sy, incy });
-
-    // Output
-    isClose(result._ret, 17);
-    isEqual(result.n, 2);
-    isClose(result.sx, [1, 2, 3, 4]);
-    isEqual(result.incx, -1);
-    isClose(result.sy, [5, 6, 7, 8]);
-    isEqual(result.incy, -1);
-    isPerf(time);
-  });
-
-  it('Case 5: n{2},sx[4],incx{-1},sy[4],incy{1}', () => {
-    // Input
-    n = 2;
-    sx = [1, 2, 3, 4];
-    incx = -1;
-    sy = [5, 6, 7, 8];
-    incy = 1;
-
-    // Apply routine
-    const { result, time } = perf(dsdot, { n, sx, incx, sy, incy });
-
-    // Output
-    isClose(result._ret, 16);
-    isEqual(result.n, 2);
-    isClose(result.sx, [1, 2, 3, 4]);
-    isEqual(result.incx, -1);
-    isClose(result.sy, [5, 6, 7, 8]);
-    isEqual(result.incy, 1);
-    isPerf(time);
+    // Run test
+    it(`Case ${++i}: n{${n}},sx[${sx.length}],incx{${incx}},sy[${sy.length}],incy{${incy}}`, () => {
+      isEqual(result._ret, _ret);
+      isEqual(result.n, n);
+      isEqual(result.sx, sx);
+      isEqual(result.incx, incx);
+      isEqual(result.sy, sy);
+      isEqual(result.incy, incy);
+      isPerf(time);
+    });
   });
 });
