80183f91ee1507bab12a64a0c76ed44e27b5f1a3
Nicholas Ng <onenylxus@gmail.com>
2024-07-18 21:33:00 +0800

    Isolate test data from CCOPY routine test

--- tests/routines/1c/ccopy.test.ts
+++ tests/routines/1c/ccopy.test.ts
@@ -1,81 +1,31 @@
 // Import
 import { isClose, isEqual, isPerf, isUndefined } from '../../testers';
 import blas from '../../../src/index';
+import data from '../../data/1c/ccopy.json';
 import perf from '../../../utils/performance';
-import _C from '../../../utils/complex';
-
-// Routine snippet
-const { ccopy } = blas;
-
-// Define test variables
-let n: number;
-let cx: Dual[];
-let incx: number;
-let cy: Dual[];
-let incy: number;
 
 // CCOPY routine test
 describe('CCOPY routine test', () => {
-  it('Case 1: n{4},cx[4],incx{1},cy[4],incy{1}', () => {
-    // Input
-    n = 4;
-    cx = [_C(1, 5), _C(2, 6), _C(3, 7), _C(4, 8)];
-    incx = 1;
-    cy = [_C(0, 0), _C(0, 0), _C(0, 0), _C(0, 0)];
-    incy = 1;
+  data.forEach((node, i) => {
+    const n = node.input.n;
+    const cx = node.input.cx;
+    const incx = node.input.incx;
+    const cy = node.input.cy;
+    const incy = node.input.incy;
 
-    // Apply routine
-    const { result, time } = perf(ccopy, { n, cx, incx, cy, incy });
+    const _cy = node.output.cy;
 
-    // Output
-    isUndefined(result._ret);
-    isEqual(result.n, 4);
-    isClose(result.cx, [_C(1, 5), _C(2, 6), _C(3, 7), _C(4, 8)]);
-    isEqual(result.incx, 1);
-    isClose(result.cy, [_C(1, 5), _C(2, 6), _C(3, 7), _C(4, 8)]);
-    isEqual(result.incy, 1);
-    isPerf(time);
-  });
+    const { result, time } = perf(blas.ccopy, { n, cx, incx, cy, incy });
 
-  it('Case 2: n{4},cx[4],incx{-1},cy[4],incy{-1}', () => {
-    // Input
-    n = 4;
-    cx = [_C(1, 5), _C(2, 6), _C(3, 7), _C(4, 8)];
-    incx = -1;
-    cy = [_C(0, 0), _C(0, 0), _C(0, 0), _C(0, 0)];
-    incy = -1;
-
-    // Apply routine
-    const { result, time } = perf(ccopy, { n, cx, incx, cy, incy });
-
-    // Output
-    isUndefined(result._ret);
-    isEqual(result.n, 4);
-    isClose(result.cx, [_C(1, 5), _C(2, 6), _C(3, 7), _C(4, 8)]);
-    isEqual(result.incx, -1);
-    isClose(result.cy, [_C(1, 5), _C(2, 6), _C(3, 7), _C(4, 8)]);
-    isEqual(result.incy, -1);
-    isPerf(time);
-  });
-
-  it('Case 3: n{0},cx[4],incx{1},cy[4],incy{1}', () => {
-    // Input
-    n = 0;
-    cx = [_C(1, 5), _C(2, 6), _C(3, 7), _C(4, 8)];
-    incx = 1;
-    cy = [_C(0, 0), _C(0, 0), _C(0, 0), _C(0, 0)];
-    incy = 1;
-
-    // Apply routine
-    const { result, time } = perf(ccopy, { n, cx, incx, cy, incy });
-
-    // Output
-    isUndefined(result._ret);
-    isEqual(result.n, 0);
-    isClose(result.cx, [_C(1, 5), _C(2, 6), _C(3, 7), _C(4, 8)]);
-    isEqual(result.incx, 1);
-    isClose(result.cy, [_C(0, 0), _C(0, 0), _C(0, 0), _C(0, 0)]);
-    isEqual(result.incy, 1);
-    isPerf(time);
+    // Run test
+    it(`Case ${++i}: n{${n}},cx[${cx.length}],incx{${incx}},cy[${cy.length}],incy{${incy}}`, () => {
+      isUndefined(result._ret);
+      isEqual(result.n, n);
+      isClose(result.cx, cx);
+      isEqual(result.incx, incx);
+      isClose(result.cy, _cy);
+      isEqual(result.incy, incy);
+      isPerf(time);
+    });
   });
 });
